802f38f25769ad9a49d192b69af45bae7bc1cdbc
Florian SIPP <florian.sipp@outlook.com>
2019-03-25 10:17:52 +0100

    Biquad Filter Debug function for array's

--- Localizer/main.cpp
+++ Localizer/main.cpp
@@ -1,10 +1,26 @@
 #include <QtWidgets/QApplication>
 #include "localizer.h"
+#include "BiquadFilter.h"
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	Localizer w;
-	w.show();
-	return a.exec();
+	//QApplication a(argc, argv);
+	//Localizer w;
+	//w.show();
+	//return a.exec();
+
+	int signalLength = 256;
+	int samplingFreq = 256;
+
+	double* sin2Freq = new double[signalLength];
+	for (int i = 0; i < signalLength; i++)
+		sin2Freq[i] = sin((2 * 3.14 * 15 * i) / samplingFreq) + sin((2 * 3.14 * 50 * i) / samplingFreq);
+
+	Filters::BiquadFilter *fil = new Filters::BiquadFilter(50, samplingFreq, Filters::FilterType::Notch, 0.707);
+	float *dbResponse = fil->FilterDecibelResponse();
+	OutputArrayToCsv1D(dbResponse, samplingFreq, "C:/Users/micromed/Desktop/file.csv");
+
+	delete[] sin2Freq;
+	delete[] dbResponse;
+	delete fil;
 }
