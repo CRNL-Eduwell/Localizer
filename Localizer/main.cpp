70ea311ab1178035ccc93ee746a8ad69755e1e58
SIPP Florian <florian.sipp@inserm.fr>
2021-09-20 17:05:31 +0200

    Set icon in main (pointing toward qrc resources) in order to make it work on osx (and probably linux as well)

--- Localizer/main.cpp
+++ Localizer/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/Localizer/Resources/localizer.ico"));
     QDir::setCurrent(QCoreApplication::applicationDirPath());
 	Localizer w;
 	w.show();
